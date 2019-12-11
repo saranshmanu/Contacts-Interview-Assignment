f882b0533db344dfce30fc9bbb15c9af1717c73a
Saransh Mittal <saransh.mittal1998@gmail.com>
2019-12-11 19:08:19 +0530

    Init Project

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Saransh Mittal on 11/12/19.
+//  Copyright © 2019 Saransh Mittal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
