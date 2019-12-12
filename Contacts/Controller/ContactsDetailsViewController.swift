1fd9399483f80171bc5562c7b15bb897bed9c4f1
Saransh Mittal <saransh.mittal1998@gmail.com>
2019-12-12 17:42:15 +0530

    Created Contacts Details basic viewController

---
+++ Contacts/Controller/ContactsDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContactsDetailsViewController.swift
+//  Contacts
+//
+//  Created by Saransh Mittal on 12/12/19.
+//  Copyright © 2019 Saransh Mittal. All rights reserved.
+//
+
+import UIKit
+
+class ContactsDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
